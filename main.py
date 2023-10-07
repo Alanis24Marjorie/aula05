ff0abdd7499f61233789ee3f26a15cca833729e6
Alanis <146586232+Alanis24Marjorie@users.noreply.github.com>
2023-10-07 12:38:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# print ("olá Mundo")
+# input("qual seu nome?")
+# texto = "Python"
+# comprimento = len(texto)  # Isso irá retornar 6
+# x = 5
+# tipo = type(x)  # Isso irá retornar <class 'int'>:
+# for in reange(2,21,2):
+# print(range)
+
+# multiplus=range(5,50,5)
+# s=sum(multiplus)
+# print(multiplus)
+
+# palavra = "python"
+# for i in palavra:
+#   print(i)
+
+
+
+
+
+# lista=['maçã','cereja','uva']
+# for i in lista:
+  
+#   print(i[2])
+# dados = {
+#   'nome': 'Julio',
+#   'idade': 18,
+#   'endereco': 'rua 10'
+# }
+
+# for i1,i2 in dados.items():
+#   print(f'{i1} : {i2}')
+
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(matriz[0][0])
+
